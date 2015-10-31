aa7ec8ea84f756339ea528dc872df2fef47a299b
Osushi <tdu.future.i@gmail.com>
2015-10-31 22:36:24 +0900

    Create Test

--- mvc/main.go
+++ mvc/main.go
@@ -6,16 +6,26 @@ import (
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 	"github.com/zenazn/goji/web/middleware"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 var db gorm.DB
 
 func main() {
+	connect()
+	rooter(goji.DefaultMux)
+	goji.Serve()
+}
+
+func rooter(m *web.Mux) http.Handler {
+	m.Use(SuperSecure)
+
 	user := web.New()
 	goji.Handle("/user/*", user)
-
 	user.Use(middleware.SubRouter)
-	user.Use(SuperSecure)
 	user.Get("/index", UserIndex)
 	user.Get("/new", UserNew)
 	user.Post("/new", UserCreate)
@@ -23,9 +33,28 @@ func main() {
 	user.Post("/update/:id", UserUpdate)
 	user.Get("/delete/:id", UserDelete)
 
-	goji.Serve()
+	return m
 }
 
-func init() {
-	db, _ = gorm.Open("mysql", "user:pass@/practice_go_mvc?charset=utf8&parseTime=True")
+func connect() {
+	yml, err := ioutil.ReadFile("conf/db.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	t := make(map[interface{}]interface{})
+	err = yaml.Unmarshal([]byte(yml), &t)
+	if err != nil {
+		panic(err)
+	}
+	env := os.Getenv("GOJIENV")
+	if env == "" {
+		env = "development"
+	}
+
+	conn := t[env].(map[interface{}]interface{})
+	db, err = gorm.Open("mysql", conn["user"].(string)+":"+conn["password"].(string)+"@/"+conn["db"].(string)+"?charset=utf8&parseTime=True")
+	if err != nil {
+		panic(err)
+	}
 }
