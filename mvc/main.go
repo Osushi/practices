882bb2b946dfafb6eaf64a8268d37257aebce251
Osushi <tdu.future.i@gmail.com>
2015-10-31 21:16:51 +0900

    Create controller and main files

---
+++ mvc/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
+	"github.com/zenazn/goji/web/middleware"
+)
+
+var db gorm.DB
+
+func main() {
+	user := web.New()
+	goji.Handle("/user/*", user)
+
+	user.Use(middleware.SubRouter)
+	user.Use(SuperSecure)
+	user.Get("/index", UserIndex)
+	user.Get("/new", UserNew)
+	user.Post("/new", UserCreate)
+	user.Get("/edit/:id", UserEdit)
+	user.Post("/update/:id", UserUpdate)
+	user.Get("/delete/:id", UserDelete)
+
+	goji.Serve()
+}
+
+func init() {
+	db, _ := gorm.Open("mysql", "user:pass@/practice_go_mvc?charset=utf8&parseTime=True")
+}
